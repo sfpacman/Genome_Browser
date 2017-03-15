4ba55602cc69523b70eef1f8e7f23de0d543d595
csawye01 <chelseakarasawyer@gmail.com>
2017-03-15 21:19:38 +0000

    Update create.sql

--- database/src/create.sql
+++ database/src/create.sql
@@ -1,9 +1,40 @@
-CREATE DATABASE protein_struc;
+CREATE DATABASE Chromosome_8;
 
-CREATE TABLE DNA
+CREATE TABLE Gene (
+  Protein_product CHAR(50) NOT NULL,
+  Genbank_Accession VARCHAR(6) NOT NULL,
+  Chromosome_location VARCHAR(8) NOT NULL,
+  Identifier VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Identifier`)
+  );
+  
+  CREATE TABLE Sequence (
+  DNA_sequence LONGTEXT NOT NULL,
+  Amino_Acid_Seq MEDIUMTEXT NOT NULL,
+  Gene_Identifier VARCHAR(45) NOT NULL,
+  INDEX `fk_Sequence_Gene_idx` (`Gene_Identifier` ASC),
+  PRIMARY KEY (`Gene_Identifier`),
+  CONSTRAINT `fk_Sequence_Gene`
+    FOREIGN KEY (`Gene_Identifier`)
+    );
+    
+CREATE TABLE Exon (
+  Exon_Start VARCHAR(45) NOT NULL,
+  Exon_end VARCHAR(45) NOT NULL,
+  Sequence_Gene_Identifier VARCHAR(45) NOT NULL,
+  INDEX `fk_Exon_Sequence1_idx` (`Sequence_Gene_Identifier` ASC),
+  PRIMARY KEY (`Sequence_Gene_Identifier`),
+  CONSTRAINT `fk_Exon_Sequence1`
+    FOREIGN KEY (`Sequence_Gene_Identifier`)
+    REFERENCES `Chromosome_8`.`Sequence` (`Gene_Identifier`)
+    );
 
-CREATE TABLE GENE
-
-CREATE TABLE
-
-LOAD DATA INFILE "chrom_CDS_8.gz" INTO TABLE genome
+CREATE TABLE `Restriction_Enzyme` (
+  `idRestriction_Enzyme` TEXT(7) NOT NULL,
+  `Recognition_seq` MEDIUMTEXT NOT NULL,
+  `Sequence_Gene_Identifier` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idRestriction_Enzyme`),
+  INDEX `fk_Restriction_Enzyme_Sequence1_idx` (`Sequence_Gene_Identifier` ASC),
+  CONSTRAINT `fk_Restriction_Enzyme_Sequence1`
+    FOREIGN KEY (`Sequence_Gene_Identifier`)
+   );
