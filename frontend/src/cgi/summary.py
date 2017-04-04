a4294f4143d1f8442d4e7fcf7e1c0d1c09079835
sfpacman <mrericyu@gmail.com>
2017-04-04 01:58:15 +0100

    new file:   cgi/.summary.py.swo 	new file:   cgi/.summary.py.swp 	new file:   cgi/Jinja2/AUTHORS 	new file:   cgi/Jinja2/CHANGES 	new file:   cgi/Jinja2/Jinja2.egg-info/PKG-INFO 	new file:   cgi/Jinja2/Jinja2.egg-info/SOURCES.txt 	new file:   cgi/Jinja2/Jinja2.egg-info/dependency_links.txt 	new file:   cgi/Jinja2/Jinja2.egg-info/entry_points.txt 	new file:   cgi/Jinja2/Jinja2.egg-info/not-zip-safe 	new file:   cgi/Jinja2/Jinja2.egg-info/requires.txt 	new file:   cgi/Jinja2/Jinja2.egg-info/top_level.txt 	new file:   cgi/Jinja2/LICENSE 	new file:   cgi/Jinja2/MANIFEST.in 	new file:   cgi/Jinja2/Makefile 	new file:   cgi/Jinja2/PKG-INFO 	new file:   cgi/Jinja2/README.rst 	new file:   cgi/Jinja2/artwork/.DS_Store 	new file:   cgi/Jinja2/artwork/jinjalogo.svg 	new file:   cgi/Jinja2/docs/.DS_Store 	new file:   cgi/Jinja2/docs/Makefile 	new file:   cgi/Jinja2/docs/_static/.ignore 	new file:   cgi/Jinja2/docs/_static/jinja-small.png 	new file:   cgi/Jinja2/docs/_templates/sidebarintro.html 	new file:   cgi/Jinja2/docs/_templates/sidebarlogo.html 	new file:   cgi/Jinja2/docs/_themes/LICENSE 	new file:   cgi/Jinja2/docs/_themes/README 	new file:   cgi/Jinja2/docs/_themes/jinja/layout.html 	new file:   cgi/Jinja2/docs/_themes/jinja/relations.html 	new file:   cgi/Jinja2/docs/_themes/jinja/static/jinja.css_t 	new file:   cgi/Jinja2/docs/_themes/jinja/theme.conf 	new file:   cgi/Jinja2/docs/api.rst 	new file:   cgi/Jinja2/docs/cache_extension.py 	new file:   cgi/Jinja2/docs/changelog.rst

---
+++ frontend/src/cgi/summary.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+import cgi
+import cgitb
+import jinja2
+cgitb.enable()
+
+def getdummy(n):
+	items=[]
+	for i in range(0,n):
+		insert = dict( id=1000+i, name=chr(97+i)+"_object", an= chr(97+i)+str(i*1000), location = "unkown" )
+		items.append(insert)
+	return items
+
+
+
+templateLoader = jinja2.FileSystemLoader( searchpath="template" )
+templateEnv = jinja2.Environment( loader=templateLoader )
+
+tempSummary = templateEnv.get_template( "Summary.html" )
+
+items=getdummy(10)
+print(tempSummary.render(items = items))
