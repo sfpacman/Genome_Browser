dd601a091629f4d179b5a6a4a22ded2d1ad3d3db
sfpacman <mrericyu@gmail.com>
2017-04-05 16:06:47 +0100

    addding details template

--- frontend/src/cgi/summary.py
+++ frontend/src/cgi/summary.py
@@ -7,6 +7,7 @@ cgitb.enable()
 def getdummy(n):
 	items=[]
 	for i in range(0,n):
+		x = i -26 *int(i/26) if i > 26 else i
 		insert = dict( id=1000+i, name=chr(97+i)+"_object", an= chr(97+i)+str(i*1000), location = "unkown" )
 		items.append(insert)
 	return items
