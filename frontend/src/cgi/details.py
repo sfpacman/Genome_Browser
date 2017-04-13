4ea3dcde58b266542ca2944fd86528e445983c0a
sfpacman <mrericyu@gmail.com>
2017-04-13 13:00:30 +0100

    seperating all dummy function into one folder

--- frontend/src/cgi/details.py
+++ frontend/src/cgi/details.py
@@ -2,36 +2,21 @@
 import cgi
 import cgitb
 import jinja2
-import random
+import getdummy
 cgitb.enable()
 
-def getdummyenzyme():
-	enzyme=["ABC","DEF","CGI"]
-	return enzyme    
-def getdummy_entry(name):
-	i=int(random.uniform(1, 26))
-	translation= "ERENEVISSAELRLFREQVDQGPDWERGFHRINIYEVMKPPAEVVPGHLITRLLDTRLVHHNVTRWETFDVSPAVLRWTREKQPNYGLAIEVTHLHQTRTHQGQHVRISRSLPQGSGNWAQLRPLLVTFGHDGRGHALTRRRRAKRSPKHHSQRARKKNKNCRRHSLYVDFSDVGWNDWIVAPPGYQAFYCHGDCPFPLADHLNSTNHAIVQTLVNSVNSSIPKACCVPTELSAISMLYLDEYDKV"
-	DNA="GGAGGGGCCGCCGGGGAAGAGGAGGAGGAAGGAAAGAAAGAAAGCGAGGGAGGGAAAGAGGAGGAAGGAAGATGCGAGAAGGCAGAGGAGGAGGGAGGGAGGGAAGGAGCGCGGAGCCCGGCCCGGAAGCTAGGTGAGTGTGGCATCCGAGCTGAGGGACGCGAGCCTGAGACGCCGCTGCTGCTCCGGCTGAGTATCTAGCTTGTCTCCCCGATGGGATTCCCGTCCAAGCTATCTCGAGCCTGCAGCGCCACAGTCCCCGGCCCTCGCCCAGGTTCACTGCAACCGTTCAGAGGTCCCCAGGAGCTGCTGCTGGCGAGCCCGCTACTGCAGGGACCTATGGAGCCATTCCGTAGTGCCATCCCGAGCAACGCACTGCTGCAGCTTCCCTGAGCCTTTCCAGCAAGTTTGTTCAAGATTGGCTGTCAAGAATCATGGACTGTTATTATATGCCTTGTTTTCTGTCAAGACACCATGATTCCTGGTAACCGAATGCTGATGGTCGTTTTATTATGCCAAGTCCTGCTAGGAGGCGCGAGCCATGCTAGTTTGATACCTGAGACGGGGAAGAAAAAAGTCGCCGAGATTCAGGGCCACGCGGGAGGACGCCGCTCAGGGCAGAGCCATGAGCTCCTGCGGGACTTCGAGGCGACACTTCTGCAGATGTTTGGGCTGCGCCGCCGCCCGCAGCCTAGCAAGA"  
-	items = dict( id=name, p_name=chr(97+i)+"_object", ga= chr(97+i)+str(i*1000), location = "unkown", DNA=DNA, Protein = translation , DNA_C="???")
-	return items
-def getdummy(type,n):
-	items=[]
-	for i in range(0,n):
-		x = i -26 *int(i/26) if i > 26 else i
-		insert = dict( id=1000+i, name=chr(97+i)+"_object", an= chr(97+i)+str(i*1000), location = "unkown" )
-		items.append(insert)
-	return items
 
+script_path="../cgi/details.py"
 form = cgi.FieldStorage()
 if "search" in form:
-	items = getdummy_entry(form["search"].value")
+	items = getdummy.getdummy_entry(form["search"].value")
 	title = "Further information for"+ str(items["id"])
-	enzyme_list = getdummyenzyme()
+	enzyme_list = getdummy.getdummyenzyme()
 	templateLoader = jinja2.FileSystemLoader( searchpath="template" )
 	templateEnv = jinja2.Environment( loader=templateLoader )
 	tempSummary = templateEnv.get_template( "Details.html" )
 	print ("Content-Type: text/html\n")
-	print(tempSummary.render(gene = items,items=enzyme_list,title=title))
+	print(tempSummary.render(gene = items,items=enzyme_list,title=title,source_path= source_path)
 elif "enzyme" in form:
 	print ("Content-Type: text/html\n")
 	print (form["enzyme"].value)
--- frontend/src/cgi/summary.py
+++ frontend/src/cgi/summary.py
@@ -1,16 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 import cgi
 import cgitb
 import jinja2
+import getdummy
 cgitb.enable()
-
-def getdummy(n):
-	items=[]
-	for i in range(0,n):
-		x = i -26 *int(i/26) if i > 26 else i
-		insert = dict( id=1000+i, name=chr(97+i)+"_object", an= chr(97+i)+str(i*1000), location = "unkown" )
-		items.append(insert)
-	return items
 
 
 form = cgi.FieldStorage()
@@ -19,7 +12,7 @@ templateEnv = jinja2.Environment( loader=templateLoader )
 
 tempSummary = templateEnv.get_template( "Summary.html" )
 
-items=getdummy(10)
+items= getdummy.getdummy(10)
 print ("Content-Type: text/html\n")
 
 print(tempSummary.render(items = items))
