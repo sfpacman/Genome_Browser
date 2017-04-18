a2ca6145de9e5bdd0df9ba0fd6b98775ed36f19b
sfpacman <mrericyu@gmail.com>
2017-04-18 14:35:41 +0100

    add util functions and add codon table python template

--- frontend/src/cgi/compute_function.py
+++ frontend/src/cgi/compute_function.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import jinja2
+from collections import OrderedDict 
 
 def markup(seq,pos_list,tag,encapsulate=False):
 	'''Adding html markup tag for both end to the seq based on the posistion from pos_list '''
@@ -18,6 +20,21 @@ def markup(seq,pos_list,tag,encapsulate=False):
 			new_seq = char if first else new_seq+char
 	return new_seq
 
-str="hello,itisme.californiadreamingsomethingIforget"
-pos_tag=[0,4,5,12,40,56]
-print(markup(str,pos_tag,"<br>",True))	
+class codon_usage:
+	'''This class is used for displaying condon_usage from a dictionary/dictionary-like object '''
+	def __init__(self,codon_dict):
+		self.codon_d = codon_dict
+	def csv_export(self,header="codon,Frequency"):
+		r_str=header
+		for codon,freq in self.codon_d.items():
+			r_str+= "\n"+codon+","+freq
+			return r_str
+	def make_html(self,col,row):
+		templateLoader = jinja2.FileSystemLoader( searchpath="template" )
+		templateEnv = jinja2.Environment( loader=templateLoader )
+		tempCodontable = templateEnv.get_template( "table.html" )
+		return(tempCodontable.render( row =row ,codon= self.codon_d, col=col))
+
+b= OrderedDict({"A":35,"B":42,"C":23,"D":2} )
+a= codon_usage(b)
+print(a.make_html(3,1))
