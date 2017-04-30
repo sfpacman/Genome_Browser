28a1a7155f669c5cdb535402faed3d86179c44d2
sfpacman <mrericyu@gmail.com>
2017-04-30 14:53:37 +0100

    add drop-down menu into mainpage

--- frontend/src/cgi/summary.py
+++ frontend/src/cgi/summary.py
@@ -11,8 +11,20 @@ templateLoader = jinja2.FileSystemLoader( searchpath="template" )
 templateEnv = jinja2.Environment( loader=templateLoader )
 
 tempSummary = templateEnv.get_template( "Summary.html" )
+a=form["query"].value.lower()
+entry=a.split(":")
+query_size = len(entry)
+action_item= ["gid","protein","ga","cl"]
+if query size > 1:
+	if entry[0] in action_item:
+		action = entry[0]
+		search_item = entry[1]
+		items= getdummy.getdummy(10)
+		print ("Content-Type: text/html\n")
+		print(tempSummary.render(items = items))
+	else:
+		print ("Content-Type: text/html\n")
+		print('<p><span class="glyphicon glyphicon-alert"></span>Error: not an valid action  </p>')
 
-items= getdummy.getdummy(10)
-print ("Content-Type: text/html\n")
 
-print(tempSummary.render(items = items))
+
