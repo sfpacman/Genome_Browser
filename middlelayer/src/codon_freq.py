de6149f02231b9425900610d2af8022359f74c5c
chiar <chiara.c.cugini@gmail.com>
2017-04-30 16:47:33 +0100

    Update codon_freq.py

--- middlelayer/src/codon_freq.py
+++ middlelayer/src/codon_freq.py
@@ -41,3 +41,54 @@ for codon in query:
 
 for codon, frequency in freq.iteritems():
     print (codon, frequency)
+    
+    
+    
+    
+    ---------------------
+    
+from collections import defaultdict
+
+seq = "AAAAAA"
+
+
+#codonsd
+codons = (('GCU', 'GCC', 'GCA', 'GCG'),
+          ('UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'),
+          ('CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'),
+          ('AAA', 'AAG'), ('AAU', 'AAC'), ('GAU', 'GAC'),
+          ('UUU', 'UUC'), ('UGU', 'UGC'), ('CCU', 'CCC', 'CCA', 'CCG'),
+          ('CAA', 'CAG'), ('UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'),
+          ('GAA', 'GAG'), ('ACU', 'ACC', 'ACA', 'ACG'),
+          ('GGU', 'GGC', 'GGA', 'GGG'), ('CAU', 'CAC'), ('UAU', 'UAC'),
+          ('AUU', 'AUC', 'AUA'), ('GUU', 'GUC', 'GUA', 'GUG'),
+          ('UAA', 'UGA', 'UAG'))
+
+
+#separate them into list
+#get different start ORFs
+c_list = []
+for codes in range(0, len(seq), 3):
+    c_list.append(seq[codes:codes+3])
+
+#count codons
+#always integer
+count = defaultdict(int)
+for codon in c_list:
+    count[codon] += 1
+
+#calculate frequencies
+#create dictionary for freqs
+
+frequency = {}
+for aaa in codons:
+    total = float(sum(count[codon] for codon in aaa))
+    for codon in aaa:
+        try:
+            frequency[codon] = count[codon] / total
+        except ZeroDivisionError:
+            frequency[codon] = 0
+
+print(frequency)
+
+    
