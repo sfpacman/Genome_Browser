35e7aeb6a3f338afb38d392f3131006be487f803
sfpacman <mrericyu@gmail.com>
2017-05-02 00:17:35 +0100

    adding codon freq table cgi

--- frontend/src/cgi/details.py
+++ frontend/src/cgi/details.py
@@ -3,21 +3,32 @@ import cgi
 import cgitb
 import jinja2
 import getdummy
+import compute_function 
 cgitb.enable()
 
 script_path="../cgi/details.py"
 form = cgi.FieldStorage()
 if "search" in form:
+	an=form["search"].value
 	items = getdummy.getdummy_entry(form["search"].value)
+	#items = get_real_stuff(an)
 	title = "Further information for"+ str(items["id"])
+	items["DNA_C"]= compute_function.markup(items["DNA"],pos_list,"<mark>")
 	enzyme_list = getdummy.getdummyenzyme()
 	templateLoader = jinja2.FileSystemLoader( searchpath="template" )
 	templateEnv = jinja2.Environment( loader=templateLoader )
 	tempSummary = templateEnv.get_template( "Details.html" )
 	print ("Content-Type: text/html\n")
-	print(tempSummary.render(gene = items,items=enzyme_list,title=title,source_path= source_path)
+	print(tempSummary.render(gene = items,items=enzyme_list,title=title,source_path= source_path))
 elif "enzyme" in form:
+	#for enzyme html section 
 	print ("Content-Type: text/html\n")
-	print (form["enzyme"].value)
+	#get_pos_enzyme(seq, form["enzyme"].value)
+	enzyme = compute_function.markup(,pos_list,"^")
+	print (enzyme)
+elif "codon" in form:
+	#Codon Frequency Table 
+	#freq=getcodon(form["codon"].value)
+	print(compute_function.codon_usage(freq))
 else:
 	print ("Content-Type: text/html\n")
--- frontend/src/cgi/summary.py
+++ frontend/src/cgi/summary.py
@@ -11,14 +11,20 @@ templateLoader = jinja2.FileSystemLoader( searchpath="template" )
 templateEnv = jinja2.Environment( loader=templateLoader )
 
 tempSummary = templateEnv.get_template( "Summary.html" )
+#retrving input from the website 
 a=form["query"].value.lower()
 entry=a.split(":")
 query_size = len(entry)
+#action definition 
 action_item= ["gid","protein","ga","cl"]
+
+#generating html page
 if query size > 1:
+#if the input in not found in the action item list , error msg will show up  
 	if entry[0] in action_item:
 		action = entry[0]
 		search_item = entry[1]
+		#getrealstuff(action_itme,search_item)
 		items= getdummy.getdummy(10)
 		print ("Content-Type: text/html\n")
 		print(tempSummary.render(items = items))
